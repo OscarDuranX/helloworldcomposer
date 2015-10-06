3929b8031d91d496728e1b3e8a49f708c53b60d8
OscarDuranX <oscarduran@iesebre.com>
2015-10-06 19:43:50 +0200

    versio Nº1

---
+++ Hello.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: oscar
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+class Hello
+{
+    public function sayHello()
+    {
+        echo "Hello my World!\n";
+    }
+
+}
